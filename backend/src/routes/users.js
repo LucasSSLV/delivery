0fca89deb9597ffc0f270984ea56faa7c0ad66a8
Lucas santos da silva <lukinhas.kell@hotmail.com>
2024-09-25 17:11:29 -0300

    implementa router userRouter

---
+++ backend/src/routes/users.js
@@ -0,0 +1,14 @@
+import express from "express";
+import UserControllers from "../controllers/users";
+
+const usersRouter = express.Router();
+
+const userscontrollers = new UserControllers();
+
+usersRouter.get("/", async (req, res) => {
+  const { success, statusCode, body } = await userscontrollers.getUsers();
+
+  res.status(statusCode).json({ success, statusCode, body });
+});
+
+export default usersRouter;
